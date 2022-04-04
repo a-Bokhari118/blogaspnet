904445a03dc04b1edf5dbc0e08265a144eb3edaa
Abdulrahman Sameer Bukhari <a.bokhari118@hotmail.com>
2022-04-04 09:50:31 +0300

    :hammer: login&register setup

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace blog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<blog.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(blog.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
