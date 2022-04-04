0fc42db84709ec9ff64d3becb5b2ffaee20bb0ed
Abdulrahman Sameer Bukhari <a.bokhari118@hotmail.com>
2022-04-04 08:57:55 +0300

    :hammer: User Setup

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using blog.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(blog.Startup))]
@@ -6,9 +9,33 @@ namespace blog
 {
     public partial class Startup
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+
+        public void CreateDefaultRolesAndUsers()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            IdentityRole role = new IdentityRole();
+            if (!roleManager.RoleExists("Admins"))
+            {
+                role.Name = "Admins";
+                roleManager.Create(role);
+
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "admin";
+                user.Email = "admin@test.com";
+                var check = userManager.Create(user, "Pa$$w0rd");
+                if (check.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admins");
+                }
+            }
         }
     }
 }
